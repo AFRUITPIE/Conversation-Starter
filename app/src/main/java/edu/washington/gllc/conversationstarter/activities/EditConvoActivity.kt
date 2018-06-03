f11b1a4018f15fcbde3447e44fa997ae142a6ff0
Hayden Hong <afruitpie@gmail.com>
2018-06-02 18:23:44 -0700

    Added delete dialog for conversations

--- app/src/main/java/edu/washington/gllc/conversationstarter/activities/EditConvoActivity.kt
+++ app/src/main/java/edu/washington/gllc/conversationstarter/activities/EditConvoActivity.kt
@@ -1,14 +1,17 @@
 package edu.washington.gllc.conversationstarter.activities
 
+import android.content.DialogInterface
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import edu.washington.gllc.conversationstarter.R
 import kotlinx.android.synthetic.main.activity_edit_convo.*
+
 
 class EditConvoActivity : AppCompatActivity() {
     private var prefs: SharedPreferences? = null
@@ -23,7 +26,19 @@ class EditConvoActivity : AppCompatActivity() {
         val mAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, conversations)
         listView.adapter = mAdapter
         listView.setOnItemClickListener { parent, view, position, id ->
-            // TODO: Add option to delete
+            // 1. Instantiate an AlertDialog.Builder with its constructor
+            val builder = AlertDialog.Builder(this)
+
+            // 2. Chain together various setter methods to set the dialog characteristics
+            builder.setMessage(R.string.delete_dialog_message)
+                    .setTitle("${getString(R.string.delete_dialog_title)} \"${conversations[position]}\"?")
+            builder.setPositiveButton(R.string.delete_ok, DialogInterface.OnClickListener { dialog, dialogId ->
+                // TODO: Remove conversations[position]
+            })
+            builder.setNegativeButton(R.string.delete_cancel, DialogInterface.OnClickListener { dialog, dialogId ->
+                // Do nothing, this is fine
+            })
+            builder.create().show()
         }
 
         // TODO: Make this fab open a dialog for adding a conversation
