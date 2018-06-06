acc0ccc761b5de18fad75054914f814f18b047f6
Hayden Hong <afruitpie@gmail.com>
2018-06-05 21:26:28 -0700

    Revamped editing, switched to tabbed layout

--- app/src/main/java/edu/washington/gllc/conversationstarter/activities/MainActivity.kt
+++ app/src/main/java/edu/washington/gllc/conversationstarter/activities/MainActivity.kt
@@ -4,16 +4,16 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.support.design.widget.Snackbar
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.google.gson.Gson
 import edu.washington.gllc.conversationstarter.R
 
 class MainActivity : AppCompatActivity() {
@@ -25,35 +25,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
         prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        findViewById<FloatingActionButton>(R.id.fab_mainFragment_editConversationStarters).setOnClickListener { startActivity(Intent(this, TabbedConvoActivity::class.java)) }
+
         start()
     }
 
     override fun onResume() {
         super.onResume()
         start()
+
     }
 
     // Handles all the starting stuff like getting preferences and setting conversations
     private fun start() {
         prefs = PreferenceManager.getDefaultSharedPreferences(this)
 
-        // Create empty value JUST for the first launch of the app
-        if (prefs?.getString("convo_array", "") == "") {
-            handleConvoJson("[]")
+        // Ensure included conversations are set
+        updateConversationAtKey("convo_included", "[\"Hello\", \"Hey, long time no see! What's up?\", \"Lol what's up kiddo\", \"Hey what's up?\", \"You want to go get dinner or something soon?\", \"The mitochondria is the powerhouse of the cell\", \"Android development is pretty cool\", \"Want to get coffee tomorrow?\", \"This is from the PLACEHOLDERS!\"]")
+
+        // Add empty arrays if not already set
+        if (prefs?.getString("convo_included", "") == "") {
+            updateConversationAtKey("convo_included", "[]")
+        }
+        if (prefs?.getString("convo_online", "") == "") {
+            updateConversationAtKey("convo_online", "[]")
         }
 
-        // Load local conversations just in case no online repo is not set
-        loadLocalConvo()
-
-        // Load online repo
+        // Load online repo if there is a repo set
         if (prefs?.getString("convo_repo", "") != "") {
             loadOnlineConvo(prefs!!.getString("convo_repo", ""))
         }
 
-        // If there's no local conversation, reset it to some placeholder conversations
-        if (prefs?.getString("convo_array", "") == "[]") {
-            resetConversations()
-        }
+        Log.i("Test", prefs?.getString("convo_array", ""))
     }
 
     /**
@@ -66,7 +69,7 @@ class MainActivity : AppCompatActivity() {
         // Request a string response from the provided URL.
         val stringRequest = StringRequest(Request.Method.GET, url,
                 Response.Listener<String> { response ->
-                    handleConvoJson(response)
+                    updateConversationAtKey("convo_online", response)
                     Log.i(localClassName, "Set new normal conversation successfully")
                 },
                 Response.ErrorListener {
@@ -77,25 +80,19 @@ class MainActivity : AppCompatActivity() {
     }
 
     /**
-     * Loads the conversations from the built-in ones
-     */
-    private fun loadLocalConvo() {
-        handleConvoJson(prefs!!.getString("convo_array", ""))
-    }
-
-    /**
      * Parses conversations from a JSON string
      * @param convoJson string version of the conversation (FROM JSON)
      */
-    private fun handleConvoJson(convoJson: String) {
-        prefs?.edit()?.putString("convo_array", convoJson)?.apply() // Override with new conversations from internet
-    }
+    private fun updateConversationAtKey(key: String, convoJson: String) {
+        prefs?.edit()?.putString(key, convoJson)?.apply() // Override with new conversations from the key
 
-    // Resets conversations to this ugly placeholder value
-    private fun resetConversations() {
-        handleConvoJson("[\"Hello\", \"Hey, long time no see! What's up?\", \"Lol what's up kiddo\", \"Hey what's up?\", \"You want to go get dinner or something soon?\", \"The mitochondria is the powerhouse of the cell\", \"Android development is pretty cool\", \"Want to get coffee tomorrow?\", \"This is from the PLACEHOLDERS!\"]")
+        // Re-compile all three conversation sources
+        var allConvos: Array<String> = Gson().fromJson(prefs?.getString("convo_local", "[]"), Array<String>::class.java) +
+                Gson().fromJson(prefs?.getString("convo_online", "[]"), Array<String>::class.java) +
+                Gson().fromJson(prefs?.getString("convo_included", "[]"), Array<String>::class.java)
+        // Override the master list with conversations
+        prefs?.edit()?.putString("convo_array", Gson().toJson(allConvos))?.apply()
     }
-
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -108,25 +105,6 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> {
                 startActivity(Intent(this, SettingsActivity::class.java))
-                return true
-            }
-            R.id.action_edit -> {
-                val intent = Intent(this, EditConvoActivity::class.java)
-                // intent.putExtra("conversations", conversations) // Add conversations to intent
-                if (prefs?.getString("convo_repo", "") != "") {
-                    // Snackbar to warn user of online repo being set,
-                    // also allows for clearing of the online repo
-                    val mySnackbar = Snackbar.make(findViewById<View>(R.id.activity_main_coordinator),
-                            R.string.snackbar_repo, Snackbar.LENGTH_LONG)
-                    mySnackbar.setAction(R.string.snackbar_action, {
-                        prefs?.edit()?.putString("convo_repo", "")?.apply()
-                        resetConversations()
-                        startActivity(intent)
-                    })
-                    mySnackbar.show()
-                } else {
-                    startActivity(intent)
-                }
                 return true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/edu/washington/gllc/conversationstarter/activities/SettingsActivity.kt
+++ app/src/main/java/edu/washington/gllc/conversationstarter/activities/SettingsActivity.kt
@@ -71,10 +71,6 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             addPreferencesFromResource(R.xml.pref_general)
             setHasOptionsMenu(true)
 
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
             bindPreferenceSummaryToValue(findPreference("convo_repo"))
             bindPreferenceSummaryToValue(findPreference("refresh_time"))
         }
